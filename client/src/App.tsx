bc3f3df4846e013195662695aeb98deb16f34307
Afneena-vk <afneenarahman98@gmail.com>
2025-05-01 17:05:31 +0530

    install react ts and tailwind css and axios,react router dom and created pages for inerfaces in the client side

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    // <div>
+      
+    // </div>
+    <div className="text-3xl font-bold text-blue-500">
+    Tailwind is working!
+  </div>
+  )
+}
+
+export default App
